16dc9299e7b371fb936ab659bcdb5a6414d084c1
andy <morrison.andrew422@gmail.com>
2023-03-03 15:33:37 -0500

    curriedSum

--- argument.js
+++ argument.js
@@ -1,8 +1,9 @@
-function sum(...args) {
+function sum() {
   let total = 0
+  console.log(arguments)
   // debugger;
-  for (let i = 0; i < args.length; i++) {
-    total += args[i];
+  for (let i = 0; i < arguments[0].length; i++) {
+    total += arguments[0][i];
   }
   return total;
 };
@@ -38,7 +39,30 @@ class Dog {
 const markov = new Cat("Markov");
 const pavlov = new Dog("Pavlov");
 
-markov.says("meow", "Ned");
+// markov.says("meow", "Ned");
 
-markov.says.myBind(pavlov, "meow", "Kush")();
-markov.says.myBind(pavlov)("meow", "a tree");
+// markov.says.myBind(pavlov, "meow", "Kush")();
+// markov.says.myBind(pavlov)("meow", "a tree");
+
+// markov.says.myBind(pavlov, "meow")("Markov");
+
+// const notMarkovSays = markov.says.myBind(pavlov);
+// notMarkovSays("meow", "me");
+
+function curriedSum(numArgs) {
+  let numbers = []
+  
+  return function _curriedSum(num) {
+    numbers.push(num);
+    debugger;
+    if (numbers.length === numArgs) {
+      return sum(numbers);
+    } else {
+      return _curriedSum;
+    }
+  }
+};
+
+
+const newSum = curriedSum(4);
+console.log(newSum(5)(30)(20)(1)); // => 56 
