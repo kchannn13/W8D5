3696b363250356ad9711213b0d108c22b51a9211
Kevin Chan <chankevin13@gmail.com>
2023-03-03 14:55:40 -0500

    myBind

--- argument.js
+++ argument.js
@@ -7,5 +7,38 @@ function sum(...args) {
   return total;
 };
 
-console.log(sum(1, 2, 3, 4)) // === 10;
-console.log(sum(1, 2, 3, 4, 5)) // === 15;
+// console.log(sum(1, 2, 3, 4)) // === 10;
+// console.log(sum(1, 2, 3, 4, 5)) // === 15;
+
+Function.prototype.myBind = function(context, ...args){
+  const otherFunc = this
+  // debugger
+  return function(...calltime){
+    return otherFunc.call(context, ...args, ...calltime);
+  }
+}
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  says(sound, person) {
+    console.log(`${this.name} says ${sound} to ${person}!`);
+    return true;
+  }
+}
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const markov = new Cat("Markov");
+const pavlov = new Dog("Pavlov");
+
+markov.says("meow", "Ned");
+
+markov.says.myBind(pavlov, "meow", "Kush")();
+markov.says.myBind(pavlov)("meow", "a tree");
